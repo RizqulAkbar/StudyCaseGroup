ef152cbf941a9192942a551cc04bc0a77ae3751f
Rizqul Akbar <rizqul141@gmail.com>
2022-01-20 12:19:30 +0700

    Update Kafka accept order Add addsaldo in finishorder

--- DriverService/Kafka/KafkaHelper.cs
+++ DriverService/Kafka/KafkaHelper.cs
@@ -1,8 +1,11 @@
 ﻿using Confluent.Kafka;
 using Confluent.Kafka.Admin;
+using DriverService.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DriverService.Kafka
@@ -61,6 +64,55 @@ namespace DriverService.Kafka
                 producer.Flush(TimeSpan.FromSeconds(10));
             }
 
+            return await Task.FromResult(succeed);
+        }
+
+        public static async Task<int> AcceptOrder(KafkaSettings settings, StudyCaseGroupContext context)
+        {
+            var succeed = 0;
+            var Serverconfig = new ConsumerConfig
+            {
+                BootstrapServers = settings.Server,
+                GroupId = "Order",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) =>
+            {
+                e.Cancel = true; // prevent the process from terminating.
+                cts.Cancel();
+            };
+            Console.WriteLine("=================Accepting Your Order=================");
+            using (var consumer = new ConsumerBuilder<string, string>(Serverconfig).Build())
+            {
+                var topics = new string[] { "Order" };
+                consumer.Subscribe(topics);
+                try
+                {
+                    var cr = consumer.Consume(cts.Token);
+                    Console.WriteLine($"Consumed record with Topic: {cr.Topic} key: {cr.Message.Key} and value: {cr.Message.Value}");
+
+                    if (cr.Topic == "Order")
+                    {
+                        Order order = JsonConvert.DeserializeObject<Order>(cr.Message.Value);
+                        order.Status = "Accepted";
+                        context.Orders.Add(order);
+                    }
+                    var cancel = await context.SaveChangesAsync();
+                    Console.WriteLine("--> Data was saved into database");
+                    Console.WriteLine("--> Order Accepted");
+
+                }
+                catch (OperationCanceledException)
+                {
+                    // Ctrl-C was pressed.
+                }
+                finally
+                {
+                    consumer.Close();
+                    succeed = 1;
+                }
+            }
             return await Task.FromResult(succeed);
         }
     }
