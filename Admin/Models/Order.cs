165a66a1f9f7a81348a6c983effe2a65d42ee163
Suzev Atsma Alboris <Suzev.Alboris@atlantica.dev>
2022-01-21 21:21:24 +0700

    new scaffold Price real on db

--- Admin/Models/Order.cs
+++ Admin/Models/Order.cs
@@ -8,15 +8,15 @@ namespace Admin.Models
     public partial class Order
     {
         public int OrderId { get; set; }
-        public int DriverId { get; set; }
+        public int? DriverId { get; set; }
         public int PenggunaId { get; set; }
         public double LatPengguna { get; set; }
         public double LongPengguna { get; set; }
-        public double LatDriver { get; set; }
-        public double LongDriver { get; set; }
+        public double? LatDriver { get; set; }
+        public double? LongDriver { get; set; }
         public double LatTujuan { get; set; }
         public double LongTujuan { get; set; }
-        public double Price { get; set; }
+        public float Price { get; set; }
         public string Status { get; set; }
         public DateTime Created { get; set; }
     }
--- Admin/Models/bootcampLearnDb5Context.cs
+++ Admin/Models/bootcampLearnDb5Context.cs
@@ -27,6 +27,11 @@ namespace Admin.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=LAPTOP-9NEN6LM2\\SQLEXPRESS;Database=bootcampLearnDb5;Trusted_Connection=True;");
+            }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -35,16 +40,9 @@ namespace Admin.Models
 
             modelBuilder.Entity<Order>(entity =>
             {
-                entity.Property(e => e.OrderId).HasColumnName("OrderID");
-
                 entity.Property(e => e.Created).HasColumnType("datetime");
 
-                entity.Property(e => e.DriverId).HasColumnName("DriverID");
-
-                entity.Property(e => e.PenggunaId).HasColumnName("PenggunaID");
-
                 entity.Property(e => e.Status)
-                    .IsRequired()
                     .HasMaxLength(50)
                     .IsUnicode(false);
             });
