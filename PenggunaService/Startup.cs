e576b8867cc93f3e9bf83fe9841413c5a0ea2237
irfhandomahendra <irfhandomahendra@gmail.com>
2022-01-21 18:12:20 +0700

    add PenggunaService to sln

--- PenggunaService/Startup.cs
+++ PenggunaService/Startup.cs
@@ -8,9 +8,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using PenggunaService.Auth;
-using PenggunaService.Data;
 using PenggunaService.GraphQL;
 using PenggunaService.Kafka;
+using PenggunaService.Models;
 
 namespace PenggunaService
 {
@@ -26,7 +26,7 @@ namespace PenggunaService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<PenggunaDbContext>(options =>
+            services.AddDbContext<bootcampLearnDb5Context>(options =>
                  options.UseSqlServer(Configuration.GetConnectionString("LocalConnection"))
             );
             services
